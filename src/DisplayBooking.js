1fc13cd8f07f383e9a39e256adc7b8e7d90624e5
suvinkumarselvaraj <vsky987@gmail.com>
2022-03-21 01:59:01 +0530

    version before adding the logic to fetch request one by one

--- src/DisplayBooking.js
+++ src/DisplayBooking.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {useStateValue} from './StateProvider';
 
-
+//1. write a logic to find the number of fields to the given query
+//2. send fetch request one by one in order to avoid receiving items on bulk 
+//3. acheive this using the count variable obtained at line 1
 function DisplayBooking() {
-
+    const [cost, setCost] = useState(0);
     var arrayOfMovies;
     const[{user},dispatch] = useStateValue();
   function fetchBooking(){
@@ -12,17 +14,22 @@ function DisplayBooking() {
     .then(data => {
     if(data.status==='success'){
       console.log('succeeded');
-      console.log(data);
+      // arrayOfMovies = JSON.parse(data);
+      // console.log('array'+arrayOfMovies);
+      // console.log(arrayOfMovies);
+      console.log(data.cost);
+      setCost(data.cost);
+      
   }
   else{
     console.log('failed');
   }
-
 })
-  }
+return (<div><p>{cost}</p></div>);}
   return (
     <div>
         {fetchBooking()}
+        {cost}
     </div>
   )
 }
