ba122f242d59fe2b0af060e1299cc78876c09090
suvinkumarselvaraj <vsky987@gmail.com>
2022-03-25 09:56:37 +0530

    home component connected to db for fetching landing page

--- src/Home.js
+++ src/Home.js
@@ -1,80 +1,52 @@
-import React from 'react'
+import React ,{useEffect,useState} from 'react'
 import MovieCards from './MovieCards';
 import './Home.css';
 import { useStateValue } from './StateProvider';
 function Home() {
   var total_movies = 0;
-function fetchInformation(){
-  fetch('http://localhost:8080/tickets/testingjson')
-  .then(data =>{
-      console.log(JSON.parse(data.array));
-      // total_movies = data.items;
-      // console.log(total_movies);
-      // console.log('success');
-      // for(var i= 0; i<items;i++)
-      // return(
-      //   <MovieCards 
-      //           id = {data.movie_id}
-      //           image = {data.movie_image}
-      //           title = {data.movie_name}
-      //           genre = {data.movie_genre}
-      //           />
-      // )
-      //console.log(JSON.parse(data.array));
-    
-  })
-  }
+ 
+  var len;
+  const [datas, setData] = useState([]);
+  useEffect(()=>{
+    const fetchData = async () => {
+      const res = await fetch(
+        'http://localhost:8080/tickets/testingjson',
+      );
+      const json = await res.json();
+        console.log(json);
+      setData(json);
+    };
+    fetchData();
+  },[setData]);
+
+    // function fetchHomeInformation(datas){
+    //   return(
+    //     <MovieCards 
+    //             id = {datas.index}
+    //             image = {datas.image}
+    //             title = "Etharkum Thuninthavan"
+    //             genre = "Action/Commercial/Thriller"
+    //             />
+    //   )
+    //}
+
    const [{user},dispatch] = useStateValue();
   return (
     <div className='home'>
         <h1>Hey {user?user :'Your Recommended Movies'}</h1>
-        <div className='home__row__1'>
-            {
-           
-              fetchInformation() 
-           
-            }
-                 {/* <MovieCards 
-                id = {2}
-                image = "https://moviegalleri.net/wp-content/uploads/2021/07/Actor-Suriya-Etharkkum-Thunindhavan-Movie-Second-Look-Poster-HD.jpg"
-                title = "Etharkum Thuninthavan"
-                genre = "Action/Commercial/Thriller"
-                /> */}
-                {/* <MovieCards 
-                id = {3}
-                image = "https://www.filmibeat.com/ph-big/2020/10/radhe-shyam_160326902360.jpg"
-                title = "Radhe Shyam"
-                genre = "Commercial/Romance"
-                />
-                <MovieCards 
-                id = {4}
-                image = "https://m.media-amazon.com/images/M/MV5BYTExZTdhY2ItNGQ1YS00NjJlLWIxMjYtZTI1MzNlMzY0OTk4XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg"
-                title = "The Batman"
-                genre = "Adventure/Thriller"
-                />
+        <div className='home__row__1'> 
+                {datas?.map(data =>(
+                                <MovieCards id = {data.id}
+                          image = {data.image}
+                          title = {data.name}
+                          genre = {data.genre} />
+          ))}
         </div>
-        <div className='home__row__1'>
-            <MovieCards 
-                id = {5}
-                image = "https://m.media-amazon.com/images/M/MV5BMjQ2ZDk2NTMtYzg0ZC00NmIyLTk2ZTQtOTkzYzI1ZjM4YTJkXkEyXkFqcGdeQXVyMjA4OTI5NDQ@._V1_.jpg"
-                title = "FIR"
-                genre = "Action/Thriller"
-                />
-                 <MovieCards 
-                id = {6}
-                image = "https://m.media-amazon.com/images/M/MV5BYWFmYWEzMzItOWNjZC00NjExLWFiMzktYWU1NzY5NjdjNzgyXkEyXkFqcGdeQXVyMTI1NDAzMzM0._V1_.jpg"
-                title = "GanguBai"
-                 genre = "Biography/Crime/Drama"
-                />
-                <MovieCards 
-                id = {7}
-                image = "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Hridayam.jpg/220px-Hridayam.jpg"
-                title = "Hridayam"
-                genre = "Commercial/Romance"
-                /> */}
-        </div>
+         
     </div>
   )
   }
+ 
+
 
 export default Home;
